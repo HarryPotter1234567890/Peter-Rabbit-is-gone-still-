6d805e65f8c206bb3a863d56aa81a59263f439a9
HarryPotter1234567890 <100985749+HarryPotter1234567890@users.noreply.github.com>
2022-05-05 19:12:49 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song100 = "";
+song200 = "";
+
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+
+function preload(){
+song100 = loadSound("music.mp3");
+song200 = loadSound("music2.mp3");
+}
+
+function play(){
+song100.play();
+song100.setVolume(1);
+song100.speed(1);
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Pose Net is not working, we will make pose net 2.");
+
+}
+
+function gotPoses(results){
+ if(results.length > 0){
+console.log(results);
+
+leftWristX = results[0].pose.leftWrist.x;
+rightWristX = results[0].pose.rightWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+rightWristY = results[0].pose.rightWrist.y;
+ }
+}
+function draw(){
+    image(video, 0, 0, 500, 500);
+}
